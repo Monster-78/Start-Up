6daa1c9e86c551040de00c67d91d8a53457eeb3a
Monster Developer <arjunsinghfzd2004@gmail.com>
2025-05-14 23:15:05 +0530

    main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 
 # Step 1: Run EDA Script (This will clean data and save cleaned_investments.csv)
 print("🔍 Running EDA script...")
+print("⏳Cool Wait Data Is Under Process.....⏰")
+print("🍿Having Some Snacks....🍿")
 subprocess.run(["python", "EDA.py"])
 
 # Step 2: Launch Streamlit Dashboard
